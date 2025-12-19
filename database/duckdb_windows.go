608fd436fb4eebd0cf3e92572a3838cce3da528d
XingKaiXin <xingkaixin@gmail.com>
2025-12-20 00:02:59 +0800

    fix: disable DuckDB on Windows builds

---
+++ database/duckdb_windows.go
@@ -0,0 +1,47 @@
+//go:build windows
+
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	"db-ferry/config"
+)
+
+type DuckDB struct {
+	db *sql.DB
+}
+
+var (
+	_ SourceDB = (*DuckDB)(nil)
+	_ TargetDB = (*DuckDB)(nil)
+)
+
+func NewDuckDB(path string) (*DuckDB, error) {
+	return nil, fmt.Errorf("duckdb is not supported on windows builds")
+}
+
+func (d *DuckDB) Close() error {
+	return fmt.Errorf("duckdb is not supported on windows builds")
+}
+
+func (d *DuckDB) Query(sql string) (*sql.Rows, error) {
+	return nil, fmt.Errorf("duckdb is not supported on windows builds")
+}
+
+func (d *DuckDB) GetRowCount(sql string) (int, error) {
+	return 0, fmt.Errorf("duckdb is not supported on windows builds")
+}
+
+func (d *DuckDB) CreateTable(tableName string, columns []ColumnMetadata) error {
+	return fmt.Errorf("duckdb is not supported on windows builds")
+}
+
+func (d *DuckDB) InsertData(tableName string, columns []ColumnMetadata, values [][]any) error {
+	return fmt.Errorf("duckdb is not supported on windows builds")
+}
+
+func (d *DuckDB) CreateIndexes(tableName string, indexes []config.IndexConfig) error {
+	return fmt.Errorf("duckdb is not supported on windows builds")
+}
